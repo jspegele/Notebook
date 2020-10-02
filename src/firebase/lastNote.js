06307a2d8a1bf7efea48e0f15bad71c4137dedcb
Justin Spegele <justin.spegele@gmail.com>
2020-10-02 14:28:38 -0400

    Removed localstorage reference

--- src/firebase/lastNote.js
+++
@@ -1,8 +0,0 @@
-export const setLastNote = id => {
-  localStorage.setItem('lastNote', JSON.stringify(id))
-}
-
-export const getLastNote = () => {
-  const localData = localStorage.getItem('lastNote')
-  return localData ? JSON.parse(localData) : null
-}
